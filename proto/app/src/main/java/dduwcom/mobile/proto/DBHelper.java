d4b58d42af6cdb171d5484b7f99f0510baf803ce
jisooHong <46811084+jisooHong@users.noreply.github.com>
2020-07-10 23:56:47 +0900

    myPage 처음
    -메인화면(커스텀필요)
    -칭호(데이터는 아직 파이어베이스에 안올림)

---
+++ proto/app/src/main/java/dduwcom/mobile/proto/DBHelper.java
@@ -0,0 +1,29 @@
+package dduwcom.mobile.proto;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DBHelper extends SQLiteOpenHelper {
+    final static String DB_NAME = "win.db";
+    public final static String TABLE_NAME = "win_table";
+    public final static String COL_ID = "_id";
+    public final static String COL_NAME = "name";
+    public final static String COL_COUNT = "count";
+
+    public DBHelper(Context context,String name,SQLiteDatabase.CursorFactory factory,int version){
+        super(context,name,null,1);
+    }
+
+    public void onCreate(SQLiteDatabase db){
+        String sql = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + "(" + COL_ID + " integer primary key, "+
+                COL_NAME + " TEXT, " + COL_COUNT + " integer);";
+        Log.d("sqlLOG",sql);
+        db.execSQL(sql);
+    }
+    public void onUpgrade(SQLiteDatabase db, int oldVer, int newVer){
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
---
+++ proto/app/src/main/java/dduwcom/mobile/proto/DataListener.java
@@ -0,0 +1,7 @@
+package dduwcom.mobile.proto;
+
+import android.widget.ImageView;
+
+public interface DataListener {
+    void dataSet(String title,int index,int category);
+}
