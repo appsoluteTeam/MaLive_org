99509ca7f301932388d428d1876498b6596e2b73
김재원 <ahswon12@naver.com>
2020-07-15 14:01:22 +0900

    cheer-up

--- app/src/main/java/com/abbsolute/ma_livu/MainActivity.java
+++ app/src/main/java/com/abbsolute/ma_livu/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO :: 메인액티비티 작성금지 (●'◡'●)
     }
 }
