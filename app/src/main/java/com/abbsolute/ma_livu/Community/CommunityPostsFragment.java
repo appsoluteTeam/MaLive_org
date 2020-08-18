811da152516133ddc7516ffea07a8293b1cd769c
seoyoon528 <seoyoon528@gmail.com>
2020-08-19 00:40:11 +0900

    CommunityPosts 수정

--- app/src/main/java/com/abbsolute/ma_livu/Community/CommunityPostsFragment.java
+++ app/src/main/java/com/abbsolute/ma_livu/Community/CommunityPostsFragment.java
@@ -30,6 +30,8 @@ public class CommunityPostsFragment extends Fragment {
     private TextView commu_writer;
     private TextView commu_date;
     private TextView commu_content;
+    private TextView commu_category;
+
 
     private Button btn_back;
     private Button btn_commu_comment;
@@ -41,8 +43,9 @@ public class CommunityPostsFragment extends Fragment {
 
         commu_title = view.findViewById(R.id.commu_title);
         commu_date = view.findViewById(R.id.commu_date);
-        commu_writer = view.findViewById(R.id.commu_writer);
+        commu_writer=view.findViewById(R.id.commu_writer);
         commu_content = view.findViewById(R.id.commu_content);
+        commu_category=view.findViewById(R.id.commu_category);
 
         btn_back = view.findViewById(R.id.btn_back);
         btn_commu_comment = view.findViewById(R.id.btn_commu_comment);
@@ -58,9 +61,18 @@ public class CommunityPostsFragment extends Fragment {
         }
 
         commu_title.setText(title);
-        commu_writer.setText(writer);
+        //commu_writer.setText(writer);
         commu_content.setText(content);
         commu_date.setText(date);
+        if (category.equals("what_eat")) {
+            commu_category.setText("뭐 먹지?");
+        }
+        if(category.equals("what_do")){
+            commu_category.setText("뭐 하지?");
+        }
+        if(category.equals("how_do")){
+            commu_category.setText("어떻게 하지?");
+        }
 
         // '뒤로가기' 버튼 눌렀을 시
         btn_back.setOnClickListener(new View.OnClickListener() {
