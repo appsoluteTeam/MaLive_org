9bbc88d0d7acf1a0ea8640e1038410fee505cc84
kdh12345 <kimdonghyeon6021@gmail.com>
2020-09-10 18:25:37 +0900

    친구목록 뒤로가기 구현

--- app/src/main/java/com/abbsolute/ma_livu/MyPage/FriendListFragment.java
+++ app/src/main/java/com/abbsolute/ma_livu/MyPage/FriendListFragment.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.abbsolute.ma_livu.R;
 
@@ -16,7 +18,18 @@ public class FriendListFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.friend_list_fragment,container,false);
-
+        Button backButton=view.findViewById(R.id.btn_back);
+        //뒤로가기 구현
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fragmentTransaction=getActivity().getSupportFragmentManager().beginTransaction();
+                MyPageFragment myPageFragment=new MyPageFragment();
+                fragmentTransaction.replace(R.id.main_frame,myPageFragment);
+                fragmentTransaction.commit();
+            }
+        });
+        //
         return view;
     }
 }
