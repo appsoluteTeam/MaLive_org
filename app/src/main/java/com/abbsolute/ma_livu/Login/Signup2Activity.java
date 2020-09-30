1a565268d82f09637a44a2bbb4a0af9515af9411
김재원 <ashwon12@naver.com>
2020-10-01 02:20:56 +0900

    닉네임 중복검사

--- app/src/main/java/com/abbsolute/ma_livu/Login/Signup2Activity.java
+++ app/src/main/java/com/abbsolute/ma_livu/Login/Signup2Activity.java
@@ -1,11 +1,13 @@
 package com.abbsolute.ma_livu.Login;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -34,8 +36,9 @@ public class Signup2Activity extends AppCompatActivity {
     //파이어스토에 저장하기 위한 변수
     private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
     private FirebaseFirestore firestore = FirebaseFirestore.getInstance();
-    private String email, password;
-    private String nicknametemp = "";
+    private String email, password,nickname;
+    private ImageButton btn_back;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,27 +49,27 @@ public class Signup2Activity extends AppCompatActivity {
         Intent intent = getIntent();
         email = intent.getStringExtra("email");
         password = intent.getStringExtra("password");
+        nickname="";
 
         tv_warning2 = (TextView) findViewById(R.id.tv_wanning2);
         et_nickname = (EditText) findViewById(R.id.et_nickname);
         btn_next2 = (Button) findViewById(R.id.btn_next2);
 
-        //TODO : 닉네임 중복 검사 , 이메일중복검사랑 알고리즘 같음
-        
-        btn_next2.setOnClickListener(new View.OnClickListener() {
+        btn_back=(ImageButton)findViewById(R.id.btn_backkey);
+        btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                isSameNickname();
-                if (nicknametemp == null) {
-                    Intent intent = new Intent(Signup2Activity.this, Signup3Activity.class);
-                    intent.putExtra("nickname", et_nickname.getText().toString());
-                    intent.putExtra("email", email);
-                    intent.putExtra("password", password);
-                    startActivity(intent);
-                }
+                startActivity(new Intent(getApplicationContext(), SignupActivity.class));
             }
         });
 
+        btn_next2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nickname=et_nickname.getText().toString();
+                isSameNickname();
+            }
+        });
     }
 
     private void isSameNickname() {
@@ -76,16 +79,40 @@ public class Signup2Activity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
-                            if (task.getResult() != null) {
-                                for (DocumentSnapshot snapshot : task.getResult()) {
-                                    Map<String, Object> shot = snapshot.getData();
-                                    nicknametemp = String.valueOf(shot.get("email"));
+                            boolean nicknameDouble = false;
+                            for (DocumentSnapshot snapshot : task.getResult()) {
+                                //겹치는 이메일이 있는것
+                                Map<String, Object> shot = snapshot.getData();
+                                tv_warning2.setText("이미 존재하는 닉네임입니다.");
+                                nicknameDouble = true;
+                                break;
+                            }
+                            if(nicknameDouble == false){//중복이 아닐 때
+                                if(isValidNickname()) {//유효성 검사
+                                    Intent intent = new Intent(Signup2Activity.this, Signup3Activity.class);
+                                    intent.putExtra("email",email);
+                                    intent.putExtra("password",password);
+                                    intent.putExtra("nickname",nickname);
+                                    startActivity(intent);
                                 }
                             }
-                        } else {
-                            nicknametemp = null;
                         }
                     }
                 });
     }
+
+    private boolean isValidNickname() {
+        if (nickname.isEmpty()) {
+            // 이메일 공백
+            tv_warning2.setText("닉네임을 입력해주세요");
+            return false;
+        }else if(nickname.length()<1 ){
+            tv_warning2.setText("2자~10자 사이의 닉네임을 설정해주세요");
+            return false;
+        }else {
+            return  true;
+        }
+    }
+
+
 }
